b3e2cc6137178dedba106f06a6976e022df974b4
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-08-12 15:24:24 +0100

    First commit

---
+++ app/resume/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+const about = {
+  title: 'About me',
+  description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam dolor assumenda qui pariatur vitae in voluptatum?',
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Maximo Signiorini"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+447354981942"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "6 years"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "maximosigniorini97@gmail.com"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Argentina"
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "Spanish, English"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Argentina"
+    },
+
+  ]
+}
+
+const Resume = () => {
+  return (
+    <div>Resume page</div>
+  )
+}
+
+export default Resume
