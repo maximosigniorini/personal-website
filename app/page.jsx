1e8e8c7ac83aefd54527c9f77235749b94f75f1b
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-08-12 16:06:04 +0100

    New main page title, button and texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
-import { FiDownload } from 'react-icons/fi'
+import { BsArrowRight } from 'react-icons/bs'
+import Link from "next/link"
 
 //components
 import Social from "@/components/Social"
@@ -12,16 +13,18 @@ const page = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Sound Designer</span>
+            <span className="text-xl">Sound Designer & Music Producer</span>
             <h1 className="h1 mb-6">Hello I'm <br /> <span className="text-accent">Maximo Signiorini</span></h1>
-            <p className="max-w-[500px] mb-9 text-white/80">Passionate and
+            <p className="max-w-[600px] mb-9 text-white/80">Passionate and
               creative in constant search for learning all aspects of sound.</p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <Link href="/portfolio">
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Portfolio</span>
+                  <BsArrowRight className="text-xl hover:-rotate-45" />
+                </Button>
+              </Link>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
