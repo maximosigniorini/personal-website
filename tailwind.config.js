41e085a39fbb4a802961f8e1228546224e77c052
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-08-12 15:38:24 +0100

    Changing font, colors and web name

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,16 +20,15 @@ module.exports = {
       xl: '1200px',
     },
     fontFamily: {
-      primary: "var(--font-jetbrainsMono)",
-      
+      primary: "var(--font-lato)",
     },
     extend: {
 
       colors: {
         primary: '#1c1c22',
         accent: {
-          DEFAULT: '#00ff99',
-          hover: '#00e187',
+          DEFAULT: '#084DFF',
+          hover: '#002fa7',
         }
       },
 
