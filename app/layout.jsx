fb91767f5fa7d17bc3cc2ed593132a21507480bf
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-08-27 16:05:56 +0100

    Fixed font

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>
+      <body className={lato.variable}>
       <Header />
       {/* <StairTransition /> */}
       <PageTransition>
