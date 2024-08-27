98ba36fbda32a8fd5a093c2ca68fc6174b197b52
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-08-27 16:42:12 +0100

    Fixed page transitions

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -30,7 +30,7 @@ const Work = () => {
   return (
     <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
       <div className="container mx-auto">
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 1, duration: 0.4, ease: 'easeIn' } }} className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.5, duration: 0.4, ease: 'easeIn' } }} className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'>
 
           {services.map((service, index)=> {
             return (
