ddf39b04b4c9eab79f5b53cbb1c57aa145869568
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-10-14 20:01:58 +0200

    Fixed typos

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"], variable: '--font-jetbrainsMono'});
+// const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"], variable: '--font-jetbrainsMono'});
 
 const lato = Lato({ subsets: ["latin"], weight: ["100", "300", "400", "700", "900"], variable: '--font-lato'});
 
@@ -19,13 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={lato.variable}>
-      <Header />
-      {/* <StairTransition /> */}
-      <PageTransition>
-      {children}
-      </PageTransition>
+        <Header />
+        {/* <StairTransition /> */}
+        <PageTransition>
+        {children}
+        </ PageTransition>
         
-        </body>
+      </body>
     </html>
   );
 }
