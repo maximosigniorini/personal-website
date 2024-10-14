a5cd0b6265afc9b1f4400d680afb3a7f23e92d0e
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-10-14 20:16:53 +0200

    Changed descriptions

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,7 @@
 "use client";
 
-const about = {
-  title: 'About me',
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam dolor assumenda qui pariatur vitae in voluptatum?',
+const details = {
+  title: 'Details',
   info: [
     {
       fieldName: "Name",
@@ -34,7 +33,7 @@ const about = {
 const experience = {
   icon: '/assets/Resume/badge.svg',
   title: 'My Experience',
-  description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt doloremque obcaecati deleniti veritatis adipisci error optio quod atque rerum.",
+  description: "Since 2016 I became deeply involved working as a music composer for films and as the years went by got more and more involved in sound design for all screen-based media.",
   items: [
     {
       company: "Ronroco Audio",
@@ -46,7 +45,7 @@ const experience = {
       company: "Freelance",
       position: "Sound Designer & Music Producer",
       duration: "2017 - present",
-      link: ""
+      link: "https://www.linkedin.com/in/maximo-signiorini/"
     }
   ]
 }
@@ -54,7 +53,6 @@ const experience = {
 const education = {
   icon: '/assets/Resume/cap.svg',
   title: 'My education',
-  description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt doloremque obcaecati deleniti veritatis adipisci error optio quod atque rerum.",
   items: [
     {
       institution: "Bournemouth University",
@@ -79,7 +77,7 @@ const education = {
 
 const skills = {
   title: "My skills",
-  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  description: "Some of the DAW's I work with alongside a wide variety of VST plugins and outboard gear.",
   skillist: [
     {
       icon: '/assets/resume/protools3.png',
@@ -119,7 +117,7 @@ const Resume = () => {
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
-            <TabsTrigger value="about">About me</TabsTrigger>
+            <TabsTrigger value="details">Details</TabsTrigger>
           </TabsList>
 
           <div className='min-h-[70vh] w-full'>
@@ -155,7 +153,6 @@ const Resume = () => {
             <TabsContent value="education" className="w-full">
               <div className='flex flex-col gap-[30px] text-center xl:text-left'>
                 <h3 className='text-4xl font-bold'>{education.title}</h3>
-                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{education.description}</p>
                 <ScrollArea className="h-[400px]">
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
                     {education.items.map((item, index) => {
@@ -205,13 +202,12 @@ const Resume = () => {
                 </ul>
               </div>
             </TabsContent>
-            {/* about me */}
-            <TabsContent value="about" className="w-full text-center xl:text-left">
+            {/* Details */}
+            <TabsContent value="details" className="w-full text-center xl:text-left">
               <div className='flex flex-col gap-[30px]'>
-                <h3 className='text-4xl font-bold'>{about.title}</h3>
-                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{about.description}</p>
+                <h3 className='text-4xl font-bold'>{details.title}</h3>
                 <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0'>
-                  {about.info.map((item, index) => {
+                  {details.info.map((item, index) => {
                     return (
                       <li key={index} className='flex items-center justify-center xl:justify-start gap-4'>
                         <span className='text-white/60'>{item.fieldName}</span>
