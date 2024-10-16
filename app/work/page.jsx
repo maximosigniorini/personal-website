769de8f82f59d42cbe555ad14aaef81f2f2236c5
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-10-16 20:18:39 +0200

    Whole category is now clickable

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -35,56 +35,24 @@ const Work = () => {
           animate={{ opacity: 1, transition: { delay: 0.5, duration: 0.4, ease: 'easeIn' } }} 
           className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'
         >
-          {/* Sound Design */}
-          <div className='flex-1 flex flex-col justify-center gap-6 group'>
-            <div className='w-full flex justify-between items-center'>
-              <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>
-                {services[0].num}
+          {/* Loop through services to create clickable categories */}
+          {services.map((service, index) => (
+            <Link href={service.href} key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
+              <div className='w-full flex justify-between items-center'>
+                <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>
+                  {service.num}
+                </div>
+                <div className='w-[70px] h-[70px] rounded-full bg-white/40 group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
+                  <BsArrowRight className='text-primary text-3xl'/>
+                </div>
               </div>
-              <Link href={services[0].href} className='w-[70px] h-[70px] rounded-full bg-white/40 group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
-                <BsArrowRight className='text-primary text-3xl'/>
-              </Link>
-            </div>
-            <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
-              {services[0].title}
-            </h2>
-            <p className='text-white/60'>{services[0].description}</p>
-            <div className='border-b border-white/20 w-full'></div>
-          </div>
-
-          {/* Music Composition */}
-          <div className='flex-1 flex flex-col justify-center gap-6 group'>
-            <div className='w-full flex justify-between items-center'>
-              <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>
-                {services[1].num}
-              </div>
-              <Link href={services[1].href} className='w-[70px] h-[70px] rounded-full bg-white/40 group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
-                <BsArrowRight className='text-primary text-3xl'/>
-              </Link>
-            </div>
-            <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
-              {services[1].title}
-            </h2>
-            <p className='text-white/60'>{services[1].description}</p>
-            <div className='border-b border-white/20 w-full'></div>
-          </div>
-
-          {/* Mix */}
-          <div className='flex-1 flex flex-col justify-center gap-6 group'>
-            <div className='w-full flex justify-between items-center'>
-              <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>
-                {services[2].num}
-              </div>
-              <Link href={services[2].href} className='w-[70px] h-[70px] rounded-full bg-white/40 group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
-                <BsArrowRight className='text-primary text-3xl'/>
-              </Link>
-            </div>
-            <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
-              {services[2].title}
-            </h2>
-            <p className='text-white/60'>{services[2].description}</p>
-            <div className='border-b border-white/20 w-full'></div>
-          </div>
+              <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
+                {service.title}
+              </h2>
+              <p className='text-white/60'>{service.description}</p>
+              <div className='border-b border-white/20 w-full'></div>
+            </Link>
+          ))}
         </motion.div>
       </div>
     </section>
