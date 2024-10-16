46e73ecf1baeb9e6662e966b6c471e2745d4aa2b
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-10-16 20:19:39 +0200

    Template for work categories

--- app/work/music/page.jsx
+++ app/work/music/page.jsx
@@ -1,8 +1,39 @@
+"use client";
+
+import VideoGrid from "@/components/VideoGrid";
+import { useRouter } from 'next/navigation';
+import { FaArrowLeft } from 'react-icons/fa';
+
+const musicVideos = [
+  { id: 1, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+  { id: 2, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+  { id: 3, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+];
 
 const Music = () => {
-  return (
-    <div>Music Page</div>
-  )
-}
+  const router = useRouter();
 
-export default Music
+  return (
+    <div className="container mx-auto py-12">
+      <div className="flex items-center mb-8">
+        {/* Back Button */}
+        <button
+          onClick={router.back}
+          className="flex items-center text-white hover:text-gray-300 transition-colors duration-200 mr-4 hover:text-accent-hover" 
+        >
+          <FaArrowLeft className="text-2xl" />
+        </button>
+
+        <h1 className="text-4xl font-bold text-center relative flex-grow">
+          Music Composition Projects
+          {/* <span className="block h-0.5 bg-white/60 mx-auto w-[380px] mt-2"></span> */}
+        </h1>
+      </div>
+
+      {/* Video Grid */}
+      <VideoGrid videos={musicVideos} />
+    </div>
+  );
+};
+
+export default Music;
--- app/work/sound-design/page.jsx
+++ app/work/sound-design/page.jsx
@@ -1,9 +1,39 @@
 "use client";
 
-const SoundDesign = () => {
-  return (
-    <div>Sound Design Work</div>
-  )
-}
+import VideoGrid from "@/components/VideoGrid";
+import { useRouter } from 'next/navigation';
+import { FaArrowLeft } from 'react-icons/fa';
 
-export default SoundDesign
+const soundDesignVideos = [
+  { id: 1, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+  { id: 2, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+  { id: 3, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+];
+
+const SoundDesign = () => {
+  const router = useRouter();
+
+  return (
+    <div className="container mx-auto py-12">
+      <div className="flex items-center mb-8">
+        {/* Back Button */}
+        <button
+          onClick={router.back} // You can directly call the function
+          className="flex items-center text-white hover:text-gray-300 transition-colors duration-200 mr-4 hover:text-accent-hover" 
+        >
+          <FaArrowLeft className="text-2xl" />
+        </button>
+
+        <h1 className="text-4xl font-bold text-center relative flex-grow">
+          Sound Design Projects
+          {/* <span className="block h-0.5 bg-white/60 mx-auto w-[380px] mt-2"></span> */}
+        </h1>
+      </div>
+
+      {/* Video Grid */}
+      <VideoGrid videos={soundDesignVideos} />
+    </div>
+  );
+};
+
+export default SoundDesign;
