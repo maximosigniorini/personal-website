f679e223a6563774a43e3246650419b2c63fb0b3
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-11-07 19:42:13 +0000

    Improved Section

--- app/work/music/page.jsx
+++ app/work/music/page.jsx
@@ -7,9 +7,51 @@ import { useRouter } from 'next/navigation';
 import { FaArrowLeft } from 'react-icons/fa';
 
 const musicVideos = [
-  { id: 1, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
-  { id: 2, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
-  { id: 3, url: 'https://youtu.be/_SmUsqJcNgo?si=xKTibN7LjuXHtpu-' },
+  { id: 1, 
+    title: 'Austin', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_austin_2.mp4',
+    thumbnail: '/assets/video-thumbnails/Austin.jpg' 
+  },
+  { id: 2, 
+    title: 'Austin Unlock', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_austin_unlock.mp4',
+    thumbnail: '/assets/video-thumbnails/Austin Unlock.jpg' 
+  },
+  { id: 3, 
+    title: 'Crinimo', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_crinimo.mp4',
+    thumbnail: '/assets/video-thumbnails/Crinimo.jpg' 
+  },
+  { id: 4, 
+    title: 'La Luna', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_la_luna.mp4',
+    thumbnail: '/assets/video-thumbnails/La Luna.jpg'
+  },
+  { id: 5, 
+    title: 'Lerato', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_lerato.mp4',
+    thumbnail: '/assets/video-thumbnails/Lerato.jpg' 
+  },
+  { id: 6, 
+    title: 'Nanobot', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_nanobot.mp4',
+    thumbnail: '/assets/video-thumbnails/Nanobot.jpg' 
+  },
+  { id: 7, 
+    title: 'NIO ET5', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_nio_et5.mp4',
+    thumbnail: '/assets/video-thumbnails/NIO ET5.jpg' 
+  },
+  { id: 8, 
+    title: 'Order Up', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_orderup.mp4',
+    thumbnail: '/assets/video-thumbnails/Order Up.jpg' 
+  },
+  { id: 9, 
+    title: 'Phases', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Music/mus_phases.mp4',
+    thumbnail: '/assets/video-thumbnails/Phases.jpg' 
+  },
 ];
 
 const Music = () => {
@@ -28,7 +70,6 @@ const Music = () => {
 
         <h1 className="text-4xl font-bold text-center relative flex-grow">
           Music Composition Projects
-          {/* <span className="block h-0.5 bg-white/60 mx-auto w-[380px] mt-2"></span> */}
         </h1>
       </div>
 
--- app/work/sound-design/page.jsx
+++ app/work/sound-design/page.jsx
@@ -9,77 +9,99 @@ import { FaArrowLeft } from 'react-icons/fa';
 const soundDesignVideos = [
   { id: 1, 
     title: 'London Move', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/London%20Move.mp4' 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Sound%20Design/London%20Move.mp4',
+    thumbnail: '/assets/video-thumbnails/London Move.jpg' 
   },
   { id: 2, 
-    title: 'HPE Playful inventiveness', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20Playful%20inventiveness.mp4' 
+    title: 'Climate Change', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Climate%20Change.mp4',
+    thumbnail: '/assets/video-thumbnails/Climate Change.jpg' 
   },
   { id: 3, 
-    title: 'HPE In Data we Trust', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20In%20Data%20We%20Trust.mp4' 
+    title: 'Momby', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Momby.mp4',
+    thumbnail: '/assets/video-thumbnails/Momby.jpg' 
   },
   { id: 4, 
-    title: 'Byclo', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Byclo.mp4',
-    thumbnail: '/assets/video-thumbnails/byclo.png'
+    title: 'HPE Playful inventiveness', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20Playful%20inventiveness.mp4',
+    thumbnail: '/assets/video-thumbnails/HPE Playful inventiveness.jpg' 
   },
   { id: 5, 
-    title: 'DL Motion', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/DL%20Motion.mp4' 
+    title: 'HPE In Data we Trust', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20In%20Data%20We%20Trust.mp4',
+    thumbnail: '/assets/video-thumbnails/HPE In Data we Trust.jpg' 
   },
   { id: 6, 
-    title: 'Momby', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Momby.mp4' 
+    title: 'HPE Lightness of Touch', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20Lightness%20Of%20Touch%20intro.mp4',
+    thumbnail: '/assets/video-thumbnails/HPE Lightness of Touch.jpg' 
   },
   { id: 7, 
-    title: 'Zapatillas', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Zapatillas.mp4' 
+    title: 'Byclo', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Byclo.mp4',
+    thumbnail: '/assets/video-thumbnails/Byclo.jpg'
   },
   { id: 8, 
-    title: 'HPE Lightness of Touch', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/HPE%20Lightness%20Of%20Touch%20intro.mp4' 
+    title: 'DL Motion', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/DL%20Motion.mp4',
+    thumbnail: '/assets/video-thumbnails/DL Motion.jpg' 
   },
   { id: 9, 
-    title: 'Natural', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Natural%20Master.mp4' 
+    title: 'Record', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Record.mp4',
+    thumbnail: '/assets/video-thumbnails/Record.jpg' 
   },
   { id: 10, 
-    title: 'Football', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Portfolio%20Football.mp4' 
+    title: 'Zapatillas', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Zapatillas.mp4',
+    thumbnail: '/assets/video-thumbnails/Zapatillas.jpg' 
   },
   { id: 11, 
-    title: 'Bamboo Traveller', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Bamboo%20Traveller%20Master.mp4' 
+    title: 'Natural', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Natural%20Master.mp4',
+    thumbnail: '/assets/video-thumbnails/Natural.jpg' 
   },
   { id: 12, 
-    title: 'Sweet Cocoon', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Sweet%20Cocoon.mp4' 
+    title: 'Football', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Portfolio%20Football.mp4',
+    thumbnail: '/assets/video-thumbnails/Football.jpg' 
   },
   { id: 13, 
-    title: 'Adidas', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Adidas.mp4' 
+    title: 'Bamboo Traveller', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Bamboo%20Traveller%20Master.mp4',
+    thumbnail: '/assets/video-thumbnails/Bamboo Traveller.jpg' 
   },
   { id: 14, 
-    title: 'Bamboo Traveler', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Bamboo%20Traveller%20Master.mp4' 
+    title: 'Sweet Cocoon', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Sweet%20Cocoon.mp4',
+    thumbnail: '/assets/video-thumbnails/Sweet Cocoon.jpg' 
   },
   { id: 15, 
-    title: 'Peter Tarka', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Peter%20Tarka%20Spotify.mp4' 
+    title: 'Adidas', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Adidas.mp4',
+    thumbnail: '/assets/video-thumbnails/Adidas.jpg' 
   },
   { id: 16, 
-    title: 'Livio Maraniello', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Livio%20Maraniello.mp4' 
+    title: 'Peter Tarka', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Peter%20Tarka%20Spotify.mp4',
+    thumbnail: '/assets/video-thumbnails/Peter Tarka.jpg' 
   },
   { id: 17, 
-    title: 'Buck', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Buck.mp4' 
+    title: 'Livio Maraniello', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Livio%20Maraniello.mp4',
+    thumbnail: '/assets/video-thumbnails/Livio Maraniello.jpg' 
   },
   { id: 18, 
-    title: 'Fundbox', 
-    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Fundbox.mp4' 
+    title: 'Buck', 
+    url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Buck.mp4',
+    thumbnail: '/assets/video-thumbnails/Buck.jpg' 
   },
+  // { id: 19, 
+  //   title: 'Fundbox', 
+  //   url: 'https://pub-3b208947b00644e0b2561af8dd4d5bfc.r2.dev/Fundbox.mp4',
+  //   thumbnail: '/assets/video-thumbnails/Fundbox.jpg'
+  // },
 ];
 
 const SoundDesign = () => {
