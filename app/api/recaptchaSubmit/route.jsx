6cad75894f217b8034f8c6d89d40bad2c9790c6a
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-11-14 16:23:03 -0300

    Fixing cloudfare build issue

--- app/api/recaptchaSubmit/route.jsx
+++ app/api/recaptchaSubmit/route.jsx
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+export const runtime = "edge";
+
 export async function POST(request) {
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
 
