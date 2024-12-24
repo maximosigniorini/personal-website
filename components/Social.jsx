33ade935fb54320228439c6f4e2c698409c43a0c
Maximo Signiorini <maximosigniorini97@gmail.com>
2024-12-24 11:26:30 -0300

    Changed Vimeo for Bluesky

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link"
 
 import {FaLinkedinIn, FaInstagram, FaYoutube, FaVimeo} from 'react-icons/fa' 
+import { FaBluesky } from "react-icons/fa6";
 
 const socials = [
   
   {icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/maximo-signiorini/'},
   {icon: <FaYoutube />, path: 'https://www.youtube.com/@maximosigniorini'},
-  {icon: <FaVimeo />, path: 'https://vimeo.com/user63763181'}, 
+  {icon: <FaBluesky />, path: 'https://bsky.app/profile/maximosigniorini.com'}, 
   {icon: <FaInstagram />, path: 'https://www.instagram.com/maximosigniorini/'},
 ]
 
