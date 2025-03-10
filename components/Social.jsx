638fb947fb98ab9bdfcbedcdf89d3694f44a0d7b
Maximo Signiorini <maximosigniorini97@gmail.com>
2025-03-10 15:25:20 +0000

    Changed instagram for mail

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-import {FaLinkedinIn, FaInstagram, FaYoutube, FaVimeo} from 'react-icons/fa' 
+import {FaLinkedinIn, FaInstagram, FaYoutube, FaEnvelope} from 'react-icons/fa' 
 import { FaBluesky } from "react-icons/fa6";
 
 const socials = [
@@ -8,7 +8,7 @@ const socials = [
   {icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/maximo-signiorini/'},
   {icon: <FaYoutube />, path: 'https://www.youtube.com/@maximosigniorini'},
   {icon: <FaBluesky />, path: 'https://bsky.app/profile/maximosigniorini.com'}, 
-  {icon: <FaInstagram />, path: 'https://www.instagram.com/maximosigniorini/'},
+  {icon: <FaEnvelope />, path: 'mailto:maximosigniorini97@gmail.com'},
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
