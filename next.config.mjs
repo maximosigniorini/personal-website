78286c15f66ad111c2c63644b7cf0f64b62a7bce
Maximo Signiorini <maximosigniorini97@gmail.com>
2025-10-14 19:55:43 -0300

    redirect /portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+    return [
+      {
+        source: '/portfolio',
+        destination: '/?video=portfolio',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
-
-
-// if (process.env.NODE_ENV === 'development') {
-//     await setupDevPlatform();
-//   }
